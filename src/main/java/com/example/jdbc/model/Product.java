14f4d60253967565cc28026e6eaa54a9816687c9
AnhagaBallas <mickle1212@mail.ru>
2023-10-05 14:12:21 +0300

    Second commit

--- src/main/java/com/example/jdbc/model/Product.java
+++
@@ -1,9 +0,0 @@
-package com.example.jdbc.model;
-
-public record Product(String productName) {
-    @Override
-    public String toString() {
-        return "productName='" + productName + '\'' +
-                '}';
-    }
-}
